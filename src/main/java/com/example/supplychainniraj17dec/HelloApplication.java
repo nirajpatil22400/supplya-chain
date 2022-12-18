b8bfaee4af6dc8a64b800457c7a9cef325d2ef63
NirajPatil <nirajpatil@22400.com>
2022-12-18 18:38:34 +0530

    login page

--- src/main/java/com/example/supplychainniraj17dec/HelloApplication.java
+++ src/main/java/com/example/supplychainniraj17dec/HelloApplication.java
@@ -3,6 +3,10 @@ package com.example.supplychainniraj17dec;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -10,8 +14,28 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
 
+    private GridPane loginPage(){
+        Label emailLabel = new Label("Email");
+        Label passwordLabel = new Label("Password");
+
+        TextField emailTextField = new TextField();
+        PasswordField passwordField = new PasswordField();
+
+        GridPane girdPane = new GridPane();
+
+        girdPane.add(emailLabel,0,0);
+        girdPane.add(emailTextField,1,0);
+        girdPane.add(passwordLabel,0,1);
+        girdPane.add(passwordField,1,1);
+
+        return girdPane;
+    }
+
     private Pane createContent(){
         Pane root = new Pane();
+
+        root.getChildren().addAll(loginPage());
+
         return  root;
     }
 
