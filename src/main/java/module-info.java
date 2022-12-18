81cb4668935fafb5a0ffceacdd2753319a9e9419
NirajPatil <nirajpatil@22400.com>
2022-12-18 18:20:07 +0530

    First Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.supplychainniraj17dec {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.supplychainniraj17dec to javafx.fxml;
+    exports com.example.supplychainniraj17dec;
+}
