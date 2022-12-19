b71b21336a235cf269db9d4b558e70a6df132357
NirajPatil <nirajpatil@22400.com>
2022-12-19 19:13:33 +0530

    first commit6

--- src/main/java/com/example/supplychainniraj17dec/Login.java
+++ src/main/java/com/example/supplychainniraj17dec/Login.java
@@ -1,8 +1,19 @@
 package com.example.supplychainniraj17dec;
 
+import java.sql.ResultSet;
+
 public class Login {
     public boolean customerLogin(String email, String password){
         String query = String.format(" SELECT * FROM customer WHERE email = '%s' AND password = '%s' ");
+        try{
+            DatabaseConnection dbCon = new DatabaseConnection();
+            ResultSet rs = dbCon.getQueryTable(query);
+            if(rs != null && rs.next()){
+                return true;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         return false;
     }
 }
