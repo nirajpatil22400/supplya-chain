8de782a9fd3a3ada0fbe051f5b1190fb54fc25a2
NirajPatil <nirajpatil@22400.com>
2022-12-19 20:03:44 +0530

    hexa decimal password

--- src/main/java/com/example/supplychainniraj17dec/Login.java
+++ src/main/java/com/example/supplychainniraj17dec/Login.java
@@ -1,8 +1,33 @@
 package com.example.supplychainniraj17dec;
 
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
 import java.sql.ResultSet;
 
 public class Login {
+
+    private byte[] getSHA(String input){
+        try{
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+            return messageDigest.digest(input.getBytes(StandardCharsets.UTF_8));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private String getEncryptedPassword(String password){
+        try{
+            BigInteger number = new BigInteger(1,getSHA(password));
+            StringBuilder hexString = new StringBuilder(number.toString(16));
+            return hexString.toString();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return  null;
+    }
+
     public boolean customerLogin(String email, String password){
         String query = String.format(" SELECT * FROM customer WHERE email = '%s' AND password = '%s' ",email,password);
         try{
@@ -17,8 +42,15 @@ public class Login {
         return false;
     }
 
+//    public static void main(String[] args){
+//        Login login = new Login();
+//        System.out.println(login.customerLogin("niraj@gmail.com","niraj123"));
+//    }
+
     public static void main(String[] args){
         Login login = new Login();
-        System.out.println(login.customerLogin("niraj@gmail.com","niraj123"));
+        System.out.println(login.getEncryptedPassword("abc123"));
     }
+
+
 }
