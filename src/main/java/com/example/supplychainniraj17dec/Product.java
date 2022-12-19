4058fe9673bf301a7f8531968070aa1ec6739332
NirajPatil <nirajpatil@22400.com>
2022-12-19 20:38:50 +0530

    create getter function in product page

--- src/main/java/com/example/supplychainniraj17dec/Product.java
+++ src/main/java/com/example/supplychainniraj17dec/Product.java
@@ -1,4 +1,41 @@
 package com.example.supplychainniraj17dec;
 
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 public class Product {
+    private SimpleIntegerProperty id;
+    private SimpleStringProperty name;
+    private SimpleDoubleProperty prize;
+
+    public int getId() {
+        return id.get();
+    }
+
+    public SimpleIntegerProperty idProperty() {
+        return id;
+    }
+
+    public Object getName() {
+        return name.get();
+    }
+
+    public SimpleStringProperty nameProperty() {
+        return name;
+    }
+
+    public double getPrize() {
+        return prize.get();
+    }
+
+    public SimpleDoubleProperty prizeProperty() {
+        return prize;
+    }
+
+    public  Product(int id , String name , double prize){
+        this.id = new SimpleIntegerProperty(id);
+        this.name = new SimpleStringProperty(name);
+        this.prize = new SimpleDoubleProperty(prize);
+    }
 }
--- src/main/java/com/example/supplychainniraj17dec/ProductDetail.java
+++ src/main/java/com/example/supplychainniraj17dec/ProductDetail.java
@@ -5,31 +5,5 @@ import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 
 public class ProductDetail {
-    private SimpleIntegerProperty id;
-    private SimpleStringProperty name;
-    private SimpleDoubleProperty prize;
 
-    public int getId() {
-        return id.get();
-    }
-
-    public SimpleIntegerProperty idProperty() {
-        return id;
-    }
-
-    public Object getName() {
-        return name.get();
-    }
-
-    public SimpleStringProperty nameProperty() {
-        return name;
-    }
-
-    public double getPrize() {
-        return prize.get();
-    }
-
-    public SimpleDoubleProperty prizeProperty() {
-        return prize;
-    }
 }
