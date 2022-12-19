8066c2e990a7a50565da6c2cc99459ecb75a0e18
NirajPatil <nirajpatil@22400.com>
2022-12-19 19:09:35 +0530

    first commit5

---
+++ src/main/java/com/example/supplychainniraj17dec/Login.java
@@ -0,0 +1,8 @@
+package com.example.supplychainniraj17dec;
+
+public class Login {
+    public boolean customerLogin(String email, String password){
+        String query = String.format(" SELECT * FROM customer WHERE email = '%s' AND password = '%s' ");
+        return false;
+    }
+}
